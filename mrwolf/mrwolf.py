5096bf8c818207e286b17c56fe2d580181709b83
Gyanendra Mishra <anomaly.the@gmail.com>
2016-02-24 23:25:17 +0530

    renamed ari to mr wolf

---
+++ mrwolf/mrwolf.py
@@ -0,0 +1,28 @@
+r"""
+ari is a problem solver and tries his best to solve your problems
+
+Usage:
+  ari z
+  ari b
+  ari f
+  ari t
+  ari k
+
+Options:
+  -h --help   Show this screen.
+  --version   Show version.
+
+"""
+
+import requests
+from docopt import docopt
+import json
+
+__version__ = '0.0.3'
+
+def main():
+  '''ari is a problem solver and tries his best to solve your problems'''
+  arguments = docopt(__doc__, version=__version__)
+
+if __name__ == '__main__':
+  main()
