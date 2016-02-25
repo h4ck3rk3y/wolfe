ce5a611bb8bfcc22de106d8024d503d26d91f304
Gyanendra Mishra <anomaly.the@gmail.com>
2016-02-25 17:33:28 +0530

    added setup files

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+if os.environ.get('USER', '') == 'vagrant':
+  del os.link
+
+
+setup(
+  name='wolfe',
+  version='0.0.3',
+  description='i am winston wolfe, i solve problems',
+  long_description=open('README.rst').read(),
+  author='Gyanendra Mishra',
+  author_email='anomaly.the@gmail.com',
+  license='MIT',
+  keywords=['wolfe', 'bugs', 'errrors', 'stackoverflow', 'cli', 'google'],
+  url='https://github.com/h4ck3rk3y/wolfe',
+  packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+  install_requires=[
+    'docopt>=0.6.2',
+    'requests>=2.6.0',
+    'tabulate==0.7.5'
+  ],
+  entry_points={
+    'console_scripts': [
+        'wolfe = wolfe.wolfe:main'
+    ],
+  }
+)
